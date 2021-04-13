04c66e60cca767d1a0e1ff041619ff5347848df3
eniocsj <eniocsjunior@gmail.com>
2021-04-13 12:52:03 -0300

    +1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Archery Person
 Authorization and session manager.
+
+[Github](https://github.com/archeryhq/archery-person)
+
+[Pipy](https://pypi.org/project/archery-person/)
--- archery_person/__init__.py
+++ archery_person/__init__.py
@@ -1,17 +1,115 @@
 from dotenv import load_dotenv
+from archery_auth import Auth
+from archery_cookie import Cookie
+from archery_secret import Secret
+
+
 load_dotenv()
-from archery_person.auths import Auth, create_tables
-from archery_person.cookies import Cookie
-from archery_person.persons import Person
-from archery_person.secrets import Secret
 
 
-__author__ = "Enio Climaco Sales Junior <eniocsjunior@gmail.com>"
-__version__ = "0.1.0"
-__all__ = [
-    "Auth",
-    "Cookie",
-    "Person",
-    "Secret",
-    "create_tables"
-]
+class Person:
+    """
+        Responsible for manage users and cookies.
+    """
+    __auth = Auth()
+    __cookie = Cookie()
+
+    def __set(
+        self: object,
+        data: dict
+    ) -> None:
+        self.username = data['username']
+        self.email = data['email']
+        self.enabled = data['enabled']
+        self.role = data['role']
+
+    def create(
+        self: object,
+        username: str,
+        email: str,
+        password: str
+    ) -> bool:
+        """
+            Create new person.
+        """
+        self.__auth.create(
+            username=username,
+            email=email,
+            password=password
+        )
+        return True
+
+    def login(
+        self: object,
+        username: str,
+        password: str
+    ) -> str:
+        person = self.__auth.login(
+            username,
+            password
+        )
+        if person:
+            self.__set(
+                person
+            )
+            self.cookie = self.__cookie.new(
+                person
+            )
+        return self.cookie
+
+    def renew(
+        self: object,
+        cookie: str
+    ) -> str:
+        try:
+            session = self.__cookie.renew(
+                cookie
+            )
+        except AttributeError:
+            return False
+        if session:
+            self.cookie = session
+        return self.cookie
+
+    def logout(
+        self: object,
+        username: str,
+        cookie: str
+    ) -> bool:
+        person = self.__auth.show(
+            username
+        )
+        session = self.__cookie.get(
+            cookie
+        )
+        if self.__secret.decrypt(
+            session
+        ) == person.id:
+            self.__cookie.delete(
+                cookie
+            )
+            return True
+        else:
+            return False
+
+    def change_password(
+        self: object,
+        username: str,
+        password: str
+    ) -> bool:
+        self.__auth.change_password(
+            username=username,
+            password=password
+        )
+        return True
+
+    def change_email(
+        self: object,
+        username: str,
+        email: str
+    ) -> bool:
+        self.__auth.change_email(
+            username=username,
+            email=email
+        )
+        return True
